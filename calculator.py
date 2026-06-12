4aa2b6dd3b7c10f114872360d7267a826384ee9a
Mahi Singh <mahi.twinkle2804@gmail.com>
2026-06-12 09:57:35 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,30 @@
+print("Simple Calculator")
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Choose operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+if choice == "1":
+    print("Result =", a + b)
+
+elif choice == "2":
+    print("Result =", a - b)
+
+elif choice == "3":
+    print("Result =", a * b)
+
+elif choice == "4":
+    if b != 0:
+        print("Result =", a / b)
+    else:
+        print("Cannot divide by zero")
+
+else:s
+    print("Invalid choice")
